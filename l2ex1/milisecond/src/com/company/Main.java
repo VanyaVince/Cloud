e7cfe745aaf1b509a8b21c926edc6bdaee50c8d2
Vanya <smp39@tut.by>
2017-11-17 21:42:51 +0300

    third commit

---
+++ l2ex1/milisecond/src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+public class Main {
+
+    long second_ms = 1000;
+    long minute_ms = second_ms * 60;
+    long hour_ms = minute_ms * 60;
+    long day_ms = hour_ms * 24;
+    long year_ms = day_ms * 365;
+
+    public static void main(String[] args) {
+
+        Main theApp = new Main();
+        theApp.task1(100000000);
+        theApp.task1(200000000);
+
+        theApp.task2(1005315611000L);
+
+    }
+
+    public void task1(long milicesconds) {
+        long hour = milicesconds % this.day_ms / this.hour_ms;
+        long minute = milicesconds % this.hour_ms / this.minute_ms;
+        long second = milicesconds % this.minute_ms / this.second_ms;
+
+        System.out.println(hour + ":" + minute + ":" + second);
+    }
+
+    /*
+        Миллисекунды отображают время, прошедшее с 01/01/1970 00:00:00.
+        Дописать вывод таким образом, чтобы миллисекунды прибавляли время к этой дате.
+        Так, для числа 1005315611000 вывод должен быть 11/09/2001 17:20:11.
+        Не использовать класс Date.
+    */
+
+    public void task2(long milicesconds) {
+
+        long years =  milicesconds / this.year_ms;
+        long days = milicesconds % this.year_ms / day_ms;
+
+
+        years = years + 1970;
+
+        // отнимаем кол-во високосных дней из days
+        for(int i = 1970; i <= years; i = i + 1) {
+            if (i % 4 == 0) {
+                days = days - 1;
+            }
+        }
+
+        // вычислить месяц и день
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int month = 0;
+
+        for(int i = 0; i < 12; i = i + 1) {
+            if (days > months[i]){
+                days = days - months[i];
+            } else {
+                month = i;
+            }
+        }
+
+        // прибавляем один день относительно 01/01/1970
+        days = days + 1;
+
+        long hour = milicesconds % day_ms / this.hour_ms;
+        long minute = milicesconds % this.hour_ms / this.minute_ms;
+        long second = milicesconds % this.minute_ms / this.second_ms;
+
+        System.out.println(month + days +years + hour + minute + second);
+    }
+}
+
+
+
+
---
+++ l3ex1/details/src/App.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class App {
+
+
+    //  public static Scanner in = new Scanner(System.in);
+    public static int Production_max = 1000;
+    public static int k1 = 100;
+    public static int k2 = 200;
+    public static int k3 = 300;
+    public static int price1 = 100;
+    public static int price2 = 90;
+    public static int price3 = 80;
+
+    public static void main(String[] args) {
+
+        int earnedMoney = 0;
+        int producedGoods = produceGoods();
+        int goodsCount = producedGoods;
+
+        int goodsForFirstShop = getGoodsForShop(1);
+        goodsCount -= goodsForFirstShop;
+        int goodsForSecondShop = getGoodsForShop(2);
+        goodsCount -= goodsForSecondShop;
+        int goodsForThirdShop = getGoodsForShop(3);
+        goodsCount -= goodsForThirdShop;
+
+
+        earnedMoney += sellGoods(goodsForFirstShop, 1);
+        earnedMoney += sellGoods(goodsForSecondShop, 2);
+        earnedMoney += sellGoods(goodsForThirdShop, 3);
+
+
+        int soldGoods = producedGoods - goodsCount;
+        printReport(soldGoods, earnedMoney);
+    }
+
+    public static int produceGoods() {
+        Random random = new Random(System.currentTimeMillis());
+        int produced = random.nextInt(Production_max + 1);
+        System.out.println("The plant produced " + produced + " details");
+        return produced;
+    }
+
+    public static int getGoodsForShop(int shopNumber) {
+        if (shopNumber == 1) {
+
+            return k1;
+        }
+
+        if (shopNumber == 2) {
+
+            return k2;
+        }
+
+        if (shopNumber == 3) {
+
+            return k3;
+        }
+        return 0;
+    }
+
+
+    public static int sellGoods(int countToSell, int shopNumber) {
+        int earned = 0;
+        int price = 0;
+
+        if (shopNumber == 1) {
+            price = price1;
+            //    earned = countToSell * price1;
+            //    System.out.println("Магазин купил " + countToSell + " по цене " + earned + " (цена за штуку: " + price1 +")");
+        }
+
+        if (shopNumber == 2) {
+            price = price2;
+            //    earned = countToSell * price2;
+            //    System.out.println("Магазин купил " + countToSell + " по цене " + earned + " (цена за штуку: " + price2 +")");
+        }
+
+        if (shopNumber == 3) {
+            price = price3;
+            //    earned = countToSell * price3;
+            //    System.out.println("Магазин купил " + countToSell + " по цене " + earned + " (цена за штуку: " + price3 +")");
+        }
+        earned = countToSell * price;
+        System.out.println("Магазин купил " + countToSell + " по цене " + earned + " (цена за штуку: " + price + ")");
+        return earned;
+    }
+
+    public static void printReport(int soldGoodsCount, int earnedMoney) {
+        double middlePrice = earnedMoney % soldGoodsCount;
+        System.out.println("Завод продал " + soldGoodsCount +
+                " по цене " + earnedMoney + ", средняя цена за штуку: " + middlePrice);
+    }
+
+
+}
+
+
+
+
+
+
