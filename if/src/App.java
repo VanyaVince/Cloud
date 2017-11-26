aeeb96be1a734e17ade3d5ef2d73ef8bffd03612
Vanya <smp39@tut.by>
2017-11-26 19:42:41 +0300

    ifi

--- if/src/App.java
+++ if/src/App.java
@@ -1,17 +1,58 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) {
-        int a = 3;
-        if (a > 0){
-            System.out.println("A больше 0");
-        }
-        else if (a < 0) {
-            System.out.println("A меньше 0");
-        }
-        else {
-            System.out.println("A равно 0");
-        }
-
+        task1();
+        task2();
 
     }
-}
 
+    public static void task1() {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите число");
+        int n = scn.nextInt();
+
+        if (n > 0) {
+            System.out.println("A больше 0");
+        } else if (n < 0) {
+            System.out.println("A меньше 0");
+        } else {
+            System.out.println("A равно 0");
+        }
+        System.out.println("Введите день недели");
+        int k = scn.nextInt();
+
+        if (k == 1) {
+            System.out.println("Понедельник");
+        } else if (k == 2) {
+            System.out.println("Вторник");
+        } else if (k == 3) {
+            System.out.println("Среда");
+        } else if (k == 4) {
+            System.out.println("Четверг");
+        } else if (k == 5) {
+            System.out.println("Пятница");
+        } else if (k == 6) {
+            System.out.println("Суббота");
+        } else if (k == 7) {
+            System.out.println("Воскресенье");
+        } else {
+            System.out.println("День недели не найден");
+        }
+    }
+
+    public static void task2() {
+        int sum = 0;
+        int[] Arr = {10, -5, 16, 99, -46, -1, 50};
+        for (int x = 0; x < 7; x++) {
+            System.out.println(Arr[x]);
+        }
+        for (int x = 0; x < 7; x++) {
+            if (Arr[x] > 0) {
+                x = Arr[x];
+
+            }
+            System.out.println(x);
+        }
+    }
+}
