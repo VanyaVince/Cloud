ee007f5a8c30e54dbd2baa18857090e4baafed0c
Vanya <smp39@tut.by>
2017-11-26 16:41:54 +0300

    ifi

---
+++ if/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 3;
+        if (a > 0){
+            System.out.println("A больше 0");
+        }
+        else if (a < 0) {
+            System.out.println("A меньше 0");
+        }
+        else {
+            System.out.println("A равно 0");
+        }
+
+
+    }
+}
+
