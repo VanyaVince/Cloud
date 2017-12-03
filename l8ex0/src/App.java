6ef6002f9e331f64e4c99162f04663011d116337
Vanya <smp39@tut.by>
2017-12-03 20:59:04 +0300

    Д-з

---
+++ l8ex0/src/App.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class App {
+
+    public static void main(String[] args) {
+        App task = new App();
+        // task.task1();
+        task.task2();
+//        int temper[] = {25,28,31,26,33,30,32,24,30,32};
+//
+//        double avg;
+//        int sum = 0;
+//        int n = temper.length;
+//        for (int i = 0; i < n; i++) {
+//            sum += temper[i];
+//        }
+//
+//        avg = (double)sum / n;
+//        System.out.println(avg);
+    }
+
+    public void task1() {
+        int summ = 0;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите количесвто чисел");
+        int n = scn.nextInt();
+        int[] Numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("Число массива");
+            Numbers[i] = scn.nextInt();
+            if (Numbers[i] < 0) {
+                summ += Numbers[i];
+            }
+        }
+        System.out.println(Arrays.toString(Numbers));
+        System.out.println(summ);
+
+    }
+
+    public void task2() {
+        float max = 0;
+        int summ1 = 0;
+        int summ2 = 0;
+        int summ3 = 0;
+        int summ4 = 0;
+        float avg1 = 0f;
+        float avg2 = 0f;
+        float avg3 = 0f;
+        float avg4 = 0f;
+        int[] Numbers = new int[1000];
+
+
+        for (int i = 0; i < 250; i++) {
+            Numbers[i] = ((int) (Math.random() * 500) - 180);
+            summ1 += Numbers[i];
+            avg1 = summ1 / 4;
+            if (max < Numbers[i]) {
+                max = Numbers[i];
+            }
+        }
+        //System.out.println(Arrays.toString(Numbers));
+        System.out.println("Среднее арифметическое 1-х четырех элементов массива = " + avg1);
+
+
+        for (int i = 250; i < 500; i++) {
+            Numbers[i] = ((int) (Math.random() * 500) - 180);
+            summ2 += Numbers[i];
+            avg2 = summ2 / 4;
+            if (max < Numbers[i]) {
+                max = Numbers[i];
+            }
+        }
+        //System.out.println(Arrays.toString(Numbers));
+        System.out.println("Среднее арифметическое 2-х четырех элементов массива = " + avg2);
+
+
+        for (int i = 500; i < 750; i++) {
+            Numbers[i] = ((int) (Math.random() * 500) - 180);
+            summ3 += Numbers[i];
+            avg3 = summ3 / 4;
+            if (max < Numbers[i]) {
+                max = Numbers[i];
+            }
+        }
+        //System.out.println(Arrays.toString(Numbers));
+        System.out.println("Среднее арифметическое 3-х четырех элементов массива = " + avg3);
+
+
+        for (int i = 750; i < 1000; i++) {
+            Numbers[i] = ((int) (Math.random() * 500) -180);
+            summ4 += Numbers[i];
+            avg4 = summ4 / 4;
+            if (max < Numbers[i]) {
+                max = Numbers[i];
+            }
+        }
+        //System.out.println(Arrays.toString(Numbers));
+        System.out.println("Среднее арифметическое 4-х четырех элементов массива = " + avg4);
+        System.out.println();
+        System.out.println("Высшее средне арифметическое значение 4-х массивов = " + max);
+
+
+    }
+}
+
+
